287c57f40c08e5e852b4d707d2aac5da5e3f5f3e
controlvee <adam.goerlich@gmail.com>
2021-01-28 14:49:05 -0600

    Add project files.

---
+++ ControlVee.Port.HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ControlVee.Port.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
